a2ffbda39db872a34fe9a5f96e789a4a70afb56c
Joshua Owusu-Ansah <joshua.owusu-ansah@nims.edu.gh>
2022-07-20 08:25:43 +0000

    Updated Notification
    Developers updated 15.0.2

---
+++ sh_web_notification-1/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name":   "Web Notification",
+    "author": "Softhealer Technologies",
+    "website": "https://www.softhealer.com",
+    "support": "support@softhealer.com",
+    "license": "OPL-1",
+    "category": "Website",
+    "summary": "Web Push Notification,Website Push Notification, Website Notification,Website Custom Push Notification,Website Custom Notification,Web Notify,user Backend Notification, Web News Announcement,Web Client Notification,Notify User Odoo",   
+    "description": """This module is useful to create a custom web notification. You can create and send instant web push notifications to users. You can send notifications in 3 ways, popup notification, animation & simple text notification.""",       
+    "version": "15.0.2",
+    "depends": [
+        "base", "web","bus",
+    ],
+    "application": True,
+    "data": [
+        "security/base_security.xml",
+        "security/ir.model.access.csv",
+        "views/annoucement_view.xml",
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'sh_web_notification/static/src/js/action_container.js',
+        ]
+    },
+    "images": ["static/description/background.png", ],
+    "auto_install": False,
+    "installable": True,
+    "price": 25,
+    "currency": "EUR"
+}
---
+++ sh_web_notification-1/doc/changelog.rst
@@ -0,0 +1,8 @@
+- 15.0.1 (18th Sept 2021)
+--------------------------
+
+-Initial Release
+
+15.0.2 (20th july,2022)
+
+-small bug fixed
