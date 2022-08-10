16023c758c3deed35b6ceb358c58e699f999d2b3
Albert <albertoappiah8@gmail.com>
2022-08-10 20:18:14 +0000

    service request module

---
+++ service_request/__manifest__.py
@@ -0,0 +1,25 @@
+
+
+{
+    "name": "Service Request",
+    "version": "1.0.0.0",
+    "author": "Albert",
+    "complexity": "easy",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "description": """
+    A module that allows users to send their service requests
+    """,
+    "images": [],
+    "depends": [
+                "website","portal"
+                ],
+    "data": [
+                'data/sequence.xml',
+                'security/ir.model.access.csv',
+                'views/service_request.xml',
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
---
+++ service_request/models/service_request.py
@@ -0,0 +1,56 @@
+from odoo import fields, models, api, _
+
+
+class ServiceRequest(models.Model):
+    _name = 'service.request'
+    _description = 'Service Request'
+    _rec_name = 'request_id'
+
+    STATES = {'done': [('readonly', True)]}
+
+    agency = fields.Many2one('res.partner', string='Agency', domain="[('is_company','=',True)]",
+                             required=True, states=STATES)
+    agent = fields.Many2one('res.users', string='Agent', required=True, states=STATES)
+    phone = fields.Char(string='Phone', states=STATES)
+    email = fields.Char(string='Email', states=STATES)
+    imdg_code = fields.Char(string='IMDG code / UN No.', required=True, states=STATES)
+    request_id = fields.Char(string='Request ID', required=True, copy=False, readonly=True, index=True,
+                                  default=lambda self: _('New'))
+    details_ids = fields.One2many('service.details','service_id', states=STATES)
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('submitted', 'Submitted'),
+        ('processed', 'Processed'),
+        ('done', 'Done')
+    ], default='draft')
+
+    @api.model
+    def create(self, values):
+        if values.get('request_id', _('New')) == _('New'):
+            values['request_id'] = self.env['ir.sequence'].next_by_code('service.request') or _('New')
+        return super(ServiceRequest, self).create(values)
+
+    def action_submit(self):
+        self.state = 'submitted'
+
+    def action_process(self):
+        self.state = 'processed'
+
+    def action_done(self):
+        self.state = 'done'
+
+
+class ServiceDetails(models.Model):
+    _name = "service.details"
+    _description = "Service Details"
+
+    description = fields.Char(string="Description")
+    quantity = fields.Integer(string="Quantity")
+    remarks = fields.Text(string="Remarks")
+    service_id = fields.Many2one('service.request')
+    product_id = fields.Many2one('product.template', string="Service", required=True,
+                                 domain="[('detailed_type','=','service')]")
+
+
+
+
