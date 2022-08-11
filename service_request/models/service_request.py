0741e911c0b058690245411b716d3664870aaa21
Albert <albertoappiah8@gmail.com>
2022-08-11 00:27:08 +0000

    Update service_request.py

--- service_request/models/service_request.py
+++ service_request/models/service_request.py
@@ -48,8 +48,9 @@ class ServiceDetails(models.Model):
     quantity = fields.Integer(string="Quantity")
     remarks = fields.Text(string="Remarks")
     service_id = fields.Many2one('service.request')
-    product_id = fields.Many2one('product.template', string="Service", required=True)
-    name = fields.Char(string="Service")
+    product_id = fields.Many2one('product.template', domain=[('detailed_type', '=', 'service')],
+                                 string="Service", required=True)
+
 
 
 
