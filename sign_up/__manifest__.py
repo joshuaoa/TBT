1a27b5c7b79330859949debe1494d88281e164b4
Albert <albertoappiah8@gmail.com>
2022-08-29 04:05:39 +0000

    test

---
+++ sign_up/__manifest__.py
@@ -0,0 +1,25 @@
+
+
+{
+    "name": "Sign Up",
+    "version": "1.0.0.0",
+    "author": "Albert",
+    "complexity": "easy",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "description": """
+    Adds additional fields when signing up for website
+    """,
+    "images": [],
+    "depends": [
+        "auth_signup",
+    ],
+    "data": [
+        'views/sign_up.xml',
+        'views/res_partner.xml',
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+
+}
---
+++ sign_up/controllers/__init__.py
@@ -0,0 +1,1 @@
+from . import auth_sign_up
