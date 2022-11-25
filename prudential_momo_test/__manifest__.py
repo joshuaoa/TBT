be27574dc6a4b8843f5becebc2ff5e7c265bcfd4
Albert <albertoappiah8@gmail.com>
2022-11-25 14:28:45 +0000

    testing

---
+++ prudential_momo_test/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Prudential Mobile Money Test',
+    'version': '1.0',
+    'category': 'Accounting/Payment Acquirers',
+    'depends': [],
+    'data': [
+        'views/momo_request.xml',
+    ],
+    'application': True,
+    'license': 'AGPL-3',
+}
---
+++ prudential_momo_test/models/momo_request.py
@@ -0,0 +1,42 @@
+from odoo import fields, models, api, _
+import logging
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+from odoo.exceptions import ValidationError
+
+
+_logger = logging.getLogger(__name__)
+
+
+
+class MomoRequest(models.Model):
+    _name = 'momo.request'
+    _description = 'Description'
+
+    response = fields.Text(string="Response")
+    status_code = fields.Char(string="Status Code")
+
+    def action_send_name_enquiry_request(self):
+        url = 'https://digihub.prudentialbank.com.gh/MobileMoneyPayment/api/Transaction/WalletNameEnquiry'
+        username = "momoapi.user.tbt"
+        password = "Temp123$"
+        auth = HTTPBasicAuth(username, password)
+
+        payload = json.dumps({
+            "clientId": "33D00BAF-04BD-4EE3-BCC6-59AB0AD8C28A",
+            "walletType": "mtn",
+            "walletNumber": "233557666857",
+        })
+
+        headers = {
+            "Username": "momoapi.user.tbt",
+            "Password": "Temp123$",
+        }
+
+        response = requests.post(url, data=payload, auth=auth)
+        # response = requests.request("POST", url, data=payload, auth=auth)
+        _logger.info(response.json)
+
+        self.response = response.json
+        self.status_code = response.status_code
